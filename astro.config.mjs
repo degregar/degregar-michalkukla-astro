1b4db93cccb84b91ddbcd5d561c0cb6ba3dc009a
Michał Kukla <degregar@gmail.com>
2024-05-15 08:18:31 +0200

    chore: upgrade packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
-import { wikiLinkPlugin } from '@stereobooster/remark-wiki-link';
+import wikiLinkPlugin from '@braindb/remark-wiki-link';
 import { bdb } from './src/lib/braindb.mjs';
 import rehypeExternalLinks from 'rehype-external-links';
 
